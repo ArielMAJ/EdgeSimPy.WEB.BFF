45f3bb766f6dea0b1b258c5f4c5854a19f7a2df8
ArielMAJ <ariel.maj@hotmail.com>
2025-05-25 17:32:06 -0300

    chore: adjust files for deployment

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,10 +3,12 @@ import "reflect-metadata";
 import { BuildSchemaOptions, buildSchema } from "type-graphql";
 import { Container } from "typedi";
 import GetCurrentUserResolver from "./resolvers/Auth/queries/currentUser";
+import LoginRequestResolver from "./resolvers/Auth/mutations/loginRequest";
+import InfoResolver from "./resolvers/Info/queries/info";
 
 const apolloServer = async () => {
   const apolloSchemaOptions: BuildSchemaOptions = {
-    resolvers: [GetCurrentUserResolver],
+    resolvers: [GetCurrentUserResolver, LoginRequestResolver, InfoResolver],
     validate: false,
     container: Container,
   };

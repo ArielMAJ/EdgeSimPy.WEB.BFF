5ce5be7daad93d8ae58483de62b4c27d7ce8ba0b
ArielMAJ <ariel.maj@hotmail.com>
2025-05-25 17:28:46 -0300

    chore: adjust files for deployment

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,22 +1,10 @@
-import "reflect-metadata";
-import path from "path";
 import { ApolloServer } from "apollo-server-express";
+import "reflect-metadata";
 import { BuildSchemaOptions, buildSchema } from "type-graphql";
 import { Container } from "typedi";
-import glob from "glob";
 import GetCurrentUserResolver from "./resolvers/Auth/queries/currentUser";
 
 const apolloServer = async () => {
-  const resolvers = path.join(__dirname, "./resolvers/**/index.{ts,js}");
-  const resolvers_vercel = path.join(__dirname, "./index.{ts,js}");
-  console.log("resolvers", resolvers);
-  const resolverFiles = glob.sync(resolvers, { absolute: true });
-  console.log("Found resolver files:", resolverFiles);
-  const allFiles = glob.sync(path.join(__dirname, "../../src/**/*.{ts,js}"), {
-    absolute: true,
-  });
-  console.log("allFiles", allFiles);
-
   const apolloSchemaOptions: BuildSchemaOptions = {
     resolvers: [GetCurrentUserResolver],
     validate: false,
